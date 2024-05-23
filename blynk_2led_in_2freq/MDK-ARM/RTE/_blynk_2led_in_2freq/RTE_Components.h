ba23cb4887686e5e042c7892549a0f61436eefee
tiendung453 <dung123qwe456@gmail.com>
2024-05-23 14:56:42 +0700

    stm32

---
+++ blynk_2led_in_2freq/MDK-ARM/RTE/_blynk_2led_in_2freq/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'blynk_2led_in_2freq' 
+ * Target:  'blynk_2led_in_2freq' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
